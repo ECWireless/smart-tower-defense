5c31896fd66f6a14ab327fb9b14b422998a990b4
ECWireless <elliott@coopallc.com>
2024-12-06 10:46:22 -0700

    Add simple server for the solidity compiler

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -13,6 +13,38 @@ export const App = () => {
 
   const [bytecode, setBytecode] = useState<string>("0x0");
   const [systemSize, setSystemSize] = useState<number>(0);
+  const [sourceCode, setSourceCode] = useState(
+    `
+    contract LogicSystem {
+        function applyStateChange(uint32 currentState) public pure returns (uint32) {
+            return currentState + 1;
+        }
+    }
+  `.trim()
+  );
+
+  const onCompileCode = useCallback(async () => {
+    try {
+      const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT;
+
+      const res = await fetch(`${API_ENDPOINT}/compile`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ sourceCode }),
+      });
+
+      if (!res.ok) {
+        throw new Error("Failed to compile code");
+      }
+
+      const bytecode = await res.text();
+      setBytecode(`0x${bytecode}`);
+    } catch (error) {
+      console.error("Error compiling code:", error);
+    }
+  }, [sourceCode]);
 
   const onGetContractSize = useCallback(async () => {
     setSystemSize(Number(await getContractSize()));
@@ -34,6 +66,21 @@ export const App = () => {
           Run state change
         </button>
       </div>
+      <br />
+      <hr />
+      <br />
+      <div>
+        <p>Compiler Version: 0.8.28</p>
+        <textarea
+          cols={100}
+          onChange={(event) => setSourceCode(event.target.value)}
+          rows={10}
+          value={sourceCode}
+        />
+      </div>
+      <button type="button" onClick={onCompileCode}>
+        Compile code
+      </button>
       <br />
       <hr />
       <br />
