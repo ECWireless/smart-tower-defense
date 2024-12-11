21fcd61e86c6d9f0ce9b8b7d68e1f52121cf6648
ECWireless <elliott@coopallc.com>
2024-12-11 06:59:11 -0700

    Improve sync progress bar

--- packages/client/src/Routes.tsx
+++ packages/client/src/Routes.tsx
@@ -2,7 +2,7 @@ import { Text, VStack } from "@chakra-ui/react";
 import { useComponentValue } from "@latticexyz/react";
 import { SyncStep } from "@latticexyz/store-sync";
 import { singletonEntity } from "@latticexyz/store-sync/recs";
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { ProgressRoot, ProgressBar } from "./components/ui/progress";
 
 import { useMUD } from "./MUDContext";
@@ -13,23 +13,18 @@ export const HOME_PATH = "/";
 export const GAMES_PATH = "/games";
 
 const AppRoutes: React.FC = () => {
-  const { pathname } = useLocation();
   const {
     components: { SyncProgress },
   } = useMUD();
 
   const syncProgress = useComponentValue(SyncProgress, singletonEntity);
 
-  if (
-    syncProgress &&
-    syncProgress.step !== SyncStep.LIVE &&
-    pathname !== HOME_PATH
-  ) {
+  if (syncProgress && syncProgress.step !== SyncStep.LIVE) {
     return (
-      <VStack justify="center" h="100%">
-        <Text>Loading {Math.round(syncProgress.percentage)}%</Text>
-        <ProgressRoot value={Math.round(syncProgress.percentage)}>
-          <ProgressBar w={{ base: "80%", sm: "50%" }} />
+      <VStack h="100vh" justifyContent="center" spaceY={4}>
+        <Text>Loading... {Math.round(syncProgress.percentage)}%</Text>
+        <ProgressRoot value={Math.round(syncProgress.percentage)} w="250px">
+          <ProgressBar />
         </ProgressRoot>
       </VStack>
     );
