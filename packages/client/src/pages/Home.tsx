1edcb997515e54e4b1c6bc11fac26c8e2af6e34e
ECWireless <elliott@coopallc.com>
2024-12-16 09:20:42 -0700

    List games on home page

--- packages/client/src/pages/Home.tsx
+++ packages/client/src/pages/Home.tsx
@@ -1,19 +1,23 @@
-import { Heading, Input, Text, VStack } from '@chakra-ui/react';
+import { Heading, HStack, Input, Tabs, Text, VStack } from '@chakra-ui/react';
+import { useEntityQuery } from '@latticexyz/react';
 import {
   Entity,
   getComponentValue,
   getComponentValueStrict,
+  Has,
 } from '@latticexyz/recs';
-import { useCallback, useEffect, useState } from 'react';
+import { encodeEntity } from '@latticexyz/store-sync/recs';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { FaPlay } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
-import { zeroAddress } from 'viem';
+import { Address, zeroAddress } from 'viem';
 
 import { Button } from '../components/ui/button';
 import { Field } from '../components/ui/field';
 import { toaster } from '../components/ui/toaster';
 import { useMUD } from '../MUDContext';
 import { GAMES_PATH } from '../Routes';
+import { type Game } from '../utils/types';
 
 export const Home = (): JSX.Element => {
   const navigate = useNavigate();
@@ -26,6 +30,41 @@ export const Home = (): JSX.Element => {
   const [username, setUsername] = useState('');
   const [usernameSaved, setUsernameSaved] = useState(false);
   const [isCreatingGame, setIsCreatingGame] = useState(false);
+
+  const games = useEntityQuery([Has(Game)]).map(entity => {
+    const _game = getComponentValueStrict(Game, entity);
+
+    const player1Entity = encodeEntity(
+      { playerAddress: 'address' },
+      { playerAddress: _game.player1Address as Address },
+    );
+    const player2Entity = encodeEntity(
+      { playerAddress: 'address' },
+      { playerAddress: _game.player2Address as Address },
+    );
+    const _player1Username = getComponentValueStrict(
+      Username,
+      player1Entity,
+    ).value;
+    const _player2Username = getComponentValueStrict(
+      Username,
+      player2Entity,
+    ).value;
+
+    return {
+      id: entity,
+      actionCount: _game.actionCount,
+      endTimestamp: _game.endTimestamp,
+      player1Address: _game.player1Address as Address,
+      player1Username: _player1Username,
+      player2Address: _game.player2Address as Address,
+      player2Username: _player2Username,
+      roundCount: _game.roundCount,
+      startTimestamp: _game.startTimestamp,
+      turn: _game.turn as Address,
+      winner: _game.winner as Address,
+    };
+  }) as Game[];
 
   const onCreateGame = useCallback(
     async (e: React.FormEvent<HTMLDivElement>) => {
@@ -84,38 +123,144 @@ export const Home = (): JSX.Element => {
     }
   }, [Username, playerEntity]);
 
+  const activeGames = useMemo(
+    () => games.filter(game => game.endTimestamp === BigInt(0)),
+    [games],
+  );
+
+  const completedGames = useMemo(
+    () => games.filter(game => game.endTimestamp !== BigInt(0)),
+    [games],
+  );
+
   return (
-    <VStack gapY={20} h="100vh" justifyContent="center" p={6}>
-      <Heading size="4xl" textAlign="center" textTransform="uppercase">
-        Smart Tower Defense
-      </Heading>
-      <VStack as="form" onSubmit={onCreateGame} spaceY={4}>
-        {usernameSaved ? (
-          <Text fontSize="lg" textAlign="center">
-            Welcome back, {username}!
-          </Text>
-        ) : (
-          <Field
-            disabled={usernameSaved}
-            label="Username"
-            required={!usernameSaved}
-            w="250px"
+    <VStack>
+      <VStack gapY={20} h="80vh" justifyContent="center" p={6}>
+        <Heading size="4xl" textAlign="center" textTransform="uppercase">
+          Smart Tower Defense
+        </Heading>
+        <VStack as="form" onSubmit={onCreateGame} spaceY={4}>
+          {usernameSaved ? (
+            <Text fontSize="lg" textAlign="center">
+              Welcome back, {username}!
+            </Text>
+          ) : (
+            <Field
+              disabled={usernameSaved}
+              label="Username"
+              required={!usernameSaved}
+              w="250px"
+            >
+              <Input
+                onChange={e => setUsername(e.target.value)}
+                value={username}
+              />
+            </Field>
+          )}
+          <Button
+            loading={isCreatingGame}
+            size="2xl"
+            type="submit"
+            variant="subtle"
           >
-            <Input
-              onChange={e => setUsername(e.target.value)}
-              value={username}
-            />
-          </Field>
-        )}
-        <Button
-          loading={isCreatingGame}
-          size="2xl"
-          type="submit"
-          variant="subtle"
-        >
-          <FaPlay color="white" />
-        </Button>
+            <FaPlay color="white" />
+          </Button>
+        </VStack>
       </VStack>
+      <Tabs.Root defaultValue="active" mb={10}>
+        <Tabs.List>
+          <Tabs.Trigger value="active">Active Games</Tabs.Trigger>
+          <Tabs.Trigger value="completed">Completed Games</Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content value="active">
+          <VStack>
+            {activeGames.length === 0 && (
+              <Text fontSize="lg" textAlign="center">
+                No active games
+              </Text>
+            )}
+            {activeGames.map(game => (
+              <HStack
+                key={game.id}
+                as="button"
+                border="1px solid white"
+                onClick={() => navigate(`${GAMES_PATH}/${game.id}`)}
+                _hover={{
+                  bgColor: 'gray.900',
+                  cursor: 'pointer',
+                }}
+              >
+                <VStack borderRight="1px solid white" gap={0} p={2} w="200px">
+                  <Text fontWeight={700}>Start Time</Text>
+                  <Text>
+                    {new Date(
+                      Number(game.startTimestamp) * 1000,
+                    ).toLocaleString()}
+                  </Text>
+                </VStack>
+                <VStack borderRight="1px solid white" gap={0} p={2} w="200px">
+                  <Text fontWeight={700}>Player 1</Text>
+                  <Text>{game.player1Username}</Text>
+                </VStack>
+                <VStack borderRight="1px solid white" gap={0} p={2} w="200px">
+                  <Text fontWeight={700}>Player 2</Text>
+                  <Text>{game.player2Username}</Text>
+                </VStack>
+                <VStack borderRight="1px solid white" gap={0} p={2} w="200px">
+                  <Text fontWeight={700}>End Time</Text>
+                  <Text>Game in progress</Text>
+                </VStack>
+              </HStack>
+            ))}
+          </VStack>
+        </Tabs.Content>
+        <Tabs.Content value="completed">
+          <VStack>
+            {completedGames.length === 0 && (
+              <Text fontSize="lg" textAlign="center">
+                No completed games
+              </Text>
+            )}
+            {completedGames.map(game => (
+              <HStack
+                key={game.id}
+                as="button"
+                border="1px solid white"
+                onClick={() => navigate(`${GAMES_PATH}/${game.id}`)}
+                _hover={{
+                  bgColor: 'gray.900',
+                  cursor: 'pointer',
+                }}
+              >
+                <VStack borderRight="1px solid white" gap={0} p={2} w="200px">
+                  <Text fontWeight={700}>Start Time</Text>
+                  <Text>
+                    {new Date(
+                      Number(game.startTimestamp) * 1000,
+                    ).toLocaleString()}
+                  </Text>
+                </VStack>
+                <VStack borderRight="1px solid white" gap={0} p={2} w="200px">
+                  <Text fontWeight={700}>Player 1</Text>
+                  <Text>{game.player1Username}</Text>
+                </VStack>
+                <VStack borderRight="1px solid white" gap={0} p={2} w="200px">
+                  <Text fontWeight={700}>Player 2</Text>
+                  <Text>{game.player2Username}</Text>
+                </VStack>
+                <VStack borderRight="1px solid white" gap={0} p={2} w="200px">
+                  <Text fontWeight={700}>End Time</Text>
+                  <Text>
+                    {new Date(
+                      Number(game.endTimestamp) * 1000,
+                    ).toLocaleString()}
+                  </Text>
+                </VStack>
+              </HStack>
+            ))}
+          </VStack>
+        </Tabs.Content>
+      </Tabs.Root>
     </VStack>
   );
 };
